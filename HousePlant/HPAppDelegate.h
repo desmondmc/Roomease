99d32d21a5c5b962819cc2b00d9f09a0fa8a1642
Desmond McNamee <desmondmcnamee@gmail.com>
2013-12-06 10:52:35 -0500

    Initial Commit

---
+++ HousePlant/HPAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  HPAppDelegate.h
+//  HousePlant
+//
+//  Created by Desmond McNamee on 12/6/2013.
+//  Copyright (c) 2013 HousePlant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ HousePlant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HousePlant
+//
+//  Created by Desmond McNamee on 12/6/2013.
+//  Copyright (c) 2013 HousePlant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPAppDelegate class]));
+    }
+}
