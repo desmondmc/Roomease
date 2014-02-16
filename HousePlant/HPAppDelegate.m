33e593ae3d5b2aa9fccb2d321a3a673d1e3a3041
Desmond McNamee <desmondmcnamee@gmail.com>
2014-02-16 16:24:37 -0500

    push notifications are working

--- HousePlant/HPAppDelegate.m
+++ HousePlant/HPAppDelegate.m
@@ -27,19 +27,25 @@
     
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     
-    // Store the deviceToken in the current installation and save it to Parse.
-    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-    [currentInstallation saveInBackground];
-    
     //[Crashlytics startWithAPIKey:kCrashlyticsKey];
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    // Register for push notifications
+    [application registerForRemoteNotificationTypes:
+     UIRemoteNotificationTypeBadge |
+     UIRemoteNotificationTypeAlert |
+     UIRemoteNotificationTypeSound];
+    
+    
     [[PFUser currentUser] fetch];
     PFUser *currentUser = [PFUser currentUser];
     if (currentUser) {
+        
+
         
         
         PFObject *house = [currentUser objectForKey:@"home"];
@@ -104,78 +110,17 @@
     }
 }
 
-#pragma mark - Core Data stack
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
+    // Store the deviceToken in the current installation and save it to Parse.
+    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+    [currentInstallation setDeviceTokenFromData:newDeviceToken];
+    [currentInstallation saveInBackground];
+}
 
-//// Returns the managed object context for the application.
-//// If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
-//- (NSManagedObjectContext *)managedObjectContext
-//{
-//    if (_managedObjectContext != nil) {
-//        return _managedObjectContext;
-//    }
-//    
-//    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
-//    if (coordinator != nil) {
-//        _managedObjectContext = [[NSManagedObjectContext alloc] init];
-//        [_managedObjectContext setPersistentStoreCoordinator:coordinator];
-//    }
-//    return _managedObjectContext;
-//}
-//
-//// Returns the managed object model for the application.
-//// If the model doesn't already exist, it is created from the application's model.
-//- (NSManagedObjectModel *)managedObjectModel
-//{
-//    if (_managedObjectModel != nil) {
-//        return _managedObjectModel;
-//    }
-//    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"HousePlant" withExtension:@"momd"];
-//    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-//    return _managedObjectModel;
-//}
-//
-//// Returns the persistent store coordinator for the application.
-//// If the coordinator doesn't already exist, it is created and the application's store added to it.
-//- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
-//{
-//    if (_persistentStoreCoordinator != nil) {
-//        return _persistentStoreCoordinator;
-//    }
-//    
-//    NSURL *storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:@"HousePlant.sqlite"];
-//    
-//    NSError *error = nil;
-//    _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
-//    if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error]) {
-//        /*
-//         Replace this implementation with code to handle the error appropriately.
-//         
-//         abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
-//         
-//         Typical reasons for an error here include:
-//         * The persistent store is not accessible;
-//         * The schema for the persistent store is incompatible with current managed object model.
-//         Check the error message to determine what the actual problem was.
-//         
-//         
-//         If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
-//         
-//         If you encounter schema incompatibility errors during development, you can reduce their frequency by:
-//         * Simply deleting the existing store:
-//         [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil]
-//         
-//         * Performing automatic lightweight migration by passing the following dictionary as the options parameter:
-//         @{NSMigratePersistentStoresAutomaticallyOption:@YES, NSInferMappingModelAutomaticallyOption:@YES}
-//         
-//         Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
-//         
-//         */
-//        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-//        abort();
-//    }    
-//    
-//    return _persistentStoreCoordinator;
-//}
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [PFPush handlePush:userInfo];
+}
 
 #pragma mark - Application's Documents directory
 
@@ -184,5 +129,7 @@
 {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
 }
+
+
 
 @end
