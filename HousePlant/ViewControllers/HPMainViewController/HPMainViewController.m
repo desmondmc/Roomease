862fb8909c2fdd3b24f967ab0c6bfa01c3233309
Desmond McNamee <desmondmcnamee@gmail.com>
2014-03-16 11:17:14 -0400

    Removed delay from updateAtHomeStatus when the main view loads.

--- HousePlant/ViewControllers/HPMainViewController/HPMainViewController.m
+++ HousePlant/ViewControllers/HPMainViewController/HPMainViewController.m
@@ -216,12 +216,12 @@
 
 - (void) receiveNotificationAppActive:(NSNotification *) notification
 {
-    double delayInSeconds = 10.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    //double delayInSeconds = 10.0;
+    //dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+    //dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
         //code to be executed on the main queue after delay
-        [[HPLocationManager sharedLocationManager] updateAtHomeStatus];
-    });
+    [[HPLocationManager sharedLocationManager] updateAtHomeStatus];
+    //});
     
 }
 
