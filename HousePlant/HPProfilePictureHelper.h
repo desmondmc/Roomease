9c08397e0acad1eae2ed3a68c27d6c357b32f6f5
Desmond McNamee <desmondmcnamee@gmail.com>
2015-01-29 15:32:17 +0000

    started work on a profile picture helper for more efficient loading of profile pics.

---
+++ HousePlant/HPProfilePictureHelper.h
@@ -0,0 +1,13 @@
+//
+//  HPProfilePictureHelper.h
+//  RoomEase
+//
+//  Created by Desmond McNamee on 2015-01-28.
+//  Copyright (c) 2015 HousePlant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPProfilePictureHelper : NSObject
+
+@end
---
+++ HousePlant/HPProfilePictureHelper.m
@@ -0,0 +1,13 @@
+//
+//  HPProfilePictureHelper.m
+//  RoomEase
+//
+//  Created by Desmond McNamee on 2015-01-28.
+//  Copyright (c) 2015 HousePlant. All rights reserved.
+//
+
+#import "HPProfilePictureHelper.h"
+
+@implementation HPProfilePictureHelper
+
+@end
